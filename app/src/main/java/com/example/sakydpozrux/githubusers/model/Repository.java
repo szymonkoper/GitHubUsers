4536422a73f6d885ae6e569b8b27c63e8dbc678b
Szymon Koper <sz.koper@gmail.com>
2017-08-07 11:22:08 +0200

    Adds Repository model

---
+++ app/src/main/java/com/example/sakydpozrux/githubusers/model/Repository.java
@@ -0,0 +1,13 @@
+package com.example.sakydpozrux.githubusers.model;
+
+/**
+ * Created by sakydpozrux on 07/08/2017.
+ */
+
+public class Repository {
+    public final String name;
+
+    public Repository(String name) {
+        this.name = name;
+    }
+}
