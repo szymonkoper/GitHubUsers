03e63413f7b67f1e98f5ab48646947f3e5599a4a
Szymon Koper <sz.koper@gmail.com>
2017-08-07 10:28:58 +0200

    Updated User model to store profile URL and repo API URL

--- app/src/main/java/com/example/sakydpozrux/githubusers/model/User.java
+++ app/src/main/java/com/example/sakydpozrux/githubusers/model/User.java
@@ -6,8 +6,12 @@ package com.example.sakydpozrux.githubusers.model;
 
 public class User {
     public final String login;
+    public final String profileUrl;
+    public final String reposUrl;
 
-    public User(String login) {
+    public User(String login, String profileUrl, String reposUrl) {
         this.login = login;
+        this.profileUrl = profileUrl;
+        this.reposUrl = reposUrl;
     }
 }
