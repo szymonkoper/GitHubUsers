619ba4ee09f0353f8bf5b12e5d3fca63faa95992
Szymon Koper <sz.koper@gmail.com>
2017-08-07 08:31:13 +0200

    Removes useless generated code

--- app/src/main/java/com/example/sakydpozrux/githubusers/UserDetailFragment.java
+++ app/src/main/java/com/example/sakydpozrux/githubusers/UserDetailFragment.java
@@ -11,28 +11,12 @@ import android.widget.TextView;
 
 import com.example.sakydpozrux.githubusers.dummy.DummyContent;
 
-/**
- * A fragment representing a single User detail screen.
- * This fragment is either contained in a {@link UserListActivity}
- * in two-pane mode (on tablets) or a {@link UserDetailActivity}
- * on handsets.
- */
 public class UserDetailFragment extends Fragment {
-    /**
-     * The fragment argument representing the item ID that this fragment
-     * represents.
-     */
+
     public static final String ARG_ITEM_ID = "item_id";
 
-    /**
-     * The dummy content this fragment is presenting.
-     */
     private DummyContent.DummyItem mItem;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
     public UserDetailFragment() {
     }
 
@@ -41,9 +25,6 @@ public class UserDetailFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
             mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
 
             Activity activity = this.getActivity();
@@ -59,7 +40,6 @@ public class UserDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.user_detail, container, false);
 
-        // Show the dummy content as text in a TextView.
         if (mItem != null) {
             ((TextView) rootView.findViewById(R.id.user_detail)).setText(mItem.details);
         }
--- app/src/main/java/com/example/sakydpozrux/githubusers/UserListActivity.java
+++ app/src/main/java/com/example/sakydpozrux/githubusers/UserListActivity.java
@@ -19,20 +19,7 @@ import com.example.sakydpozrux.githubusers.dummy.DummyContent;
 
 import java.util.List;
 
-/**
- * An activity representing a list of Users. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link UserDetailActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
- */
 public class UserListActivity extends AppCompatActivity {
-
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
     private boolean mTwoPane;
 
     @Override
@@ -58,10 +45,6 @@ public class UserListActivity extends AppCompatActivity {
         setupRecyclerView((RecyclerView) recyclerView);
 
         if (findViewById(R.id.user_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
             mTwoPane = true;
         }
     }
