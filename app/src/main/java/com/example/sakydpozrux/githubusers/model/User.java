9636dbee5f339cdc1ac37bdc9b5a5a85b6dac848
Szymon Koper <sz.koper@gmail.com>
2017-08-07 09:37:03 +0200

    Adds model for User

---
+++ app/src/main/java/com/example/sakydpozrux/githubusers/model/User.java
@@ -0,0 +1,13 @@
+package com.example.sakydpozrux.githubusers.model;
+
+/**
+ * Created by sakydpozrux on 07/08/2017.
+ */
+
+public class User {
+    public final String login;
+
+    public User(String login) {
+        this.login = login;
+    }
+}
